bc104ffacaf3da9c431306e659c17839f623ca2e
Yolimar Marin <yolimar.ma83@gmail.com>
2020-09-19 13:18:16 -0500

    add apollo and graphql

--- pages/register.js
+++ pages/register.js
@@ -1,11 +1,161 @@
-import React from 'react'
-import Layout from '../components/Layout'
-const Register = () => {
-    return(
-        <Layout>
-        <h1>Register</h1>
-        </Layout>
-    )
-}
+import React from "react";
+import Layout from "../components/Layout";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useQuery, gql } from "@apollo/client";
 
-export default Register
+const QUERY = gql`
+  query getProducts {
+    getProducts {
+      id
+      name
+      price
+    }
+  }
+`;
+
+const Register = () => {
+  const { data } = useQuery(QUERY);
+  console.log(data)
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      lastName: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required('the field "Name" is required'),
+      lastName: Yup.string().required('the field "Last Name" is required'),
+      email: Yup.string()
+        .email("the email is not valid")
+        .required('the field "Email" is required'),
+      password: Yup.string()
+        .required('the field "Password" is required')
+        .min(6, "the password must have at least 6 characters"),
+    }),
+    onSubmit: (values) => {
+      console.log("...sending");
+      console.log(values);
+    },
+  });
+
+  return (
+    <Layout>
+      <h1 className="text-center text-2xl text-white font-light">Register</h1>
+      <div className="flex justify-center mt-t">
+        <div className="w-full max-w-sm">
+          <form
+            className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+            onSubmit={formik.handleSubmit}
+          >
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="name"
+              >
+                Name
+              </label>
+              <input
+                className="shadow appereance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="name"
+                type="text"
+                placeholder="Name"
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+            </div>
+
+            {formik.touched.name && formik.errors.name ? (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                {formik.errors.name}
+              </div>
+            ) : null}
+
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="lastName"
+              >
+                Last Name
+              </label>
+              <input
+                className="shadow appereance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="lastName"
+                type="text"
+                placeholder="Last Name"
+                value={formik.values.lastName}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+            </div>
+
+            {formik.touched.lastName && formik.errors.lastName ? (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                {formik.errors.lastName}
+              </div>
+            ) : null}
+
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                className="shadow appereance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="email"
+                type="email"
+                placeholder="Email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+            </div>
+
+            {formik.touched.email && formik.errors.email ? (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                {formik.errors.email}
+              </div>
+            ) : null}
+
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="shadow appereance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="Password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+            </div>
+
+            {formik.touched.password && formik.errors.password ? (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                {formik.errors.password}
+              </div>
+            ) : null}
+
+            <input
+              className="bg-gray-800 w-full mt-5 p-2 text-white uppercase hover:bg-gray-900"
+              type="submit"
+              value="Register"
+            />
+          </form>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Register;
